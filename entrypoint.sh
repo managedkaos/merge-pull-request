497bf2c334eaafaafa73585c9f2a2ad5a982e4ae
Michael <managedkaos@users.noreply.github.com>
2019-04-09 10:07:24 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,25 +2,13 @@
 
 set -e
 
-if [ -n "$GITHUB_EVENT_PATH" ];
-then
-    EVENT_PATH=$GITHUB_EVENT_PATH
-elif [ -f ./sample_pull_request_event.json ];
-then
-    EVENT_PATH='./sample_pull_request_event.json'
-    LOCAL_TEST=true
-else
-    echo "No JSON data to process! :("
-    exit 1
-fi
-
 echo "-----------------------------------------------------------"
-echo "Processing $EVENT_PATH"
+echo "Processing $GITHUB_EVENT_PATH"
 echo "-----------------------------------------------------------"
 
 echo "### printing environment"
 env
-jq . < $EVENT_PATH
+jq . < $GITHUB_EVENT_PATH
 
 echo "### checking for PR URL"
 (jq -r ".issue.pull_request.url" "$EVENT_PATH") || exit 78
@@ -28,3 +16,4 @@ echo "### checking for PR URL"
 echo "### getting the PR number"
 PR_NUMBER=$(jq -r ".issue.number" "$GITHUB_EVENT_PATH")
 
+echo "### $PR_NUMBER"
