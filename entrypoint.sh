b43e8b111a603b00797e2e4f48fb6ee363a6890e
Michael <managedkaos@users.noreply.github.com>
2019-04-09 10:06:24 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 if [ -n "$GITHUB_EVENT_PATH" ];
 then
     EVENT_PATH=$GITHUB_EVENT_PATH
-elif [ -f ./sample_push_event.json ];
+elif [ -f ./sample_pull_request_event.json ];
 then
     EVENT_PATH='./sample_pull_request_event.json'
     LOCAL_TEST=true
@@ -28,25 +28,3 @@ echo "### checking for PR URL"
 echo "### getting the PR number"
 PR_NUMBER=$(jq -r ".issue.number" "$GITHUB_EVENT_PATH")
 
-
-URI=https://api.github.com
-API_HEADER="Accept: application/vnd.github.v3+json"
-AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
-
-pr_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER}")
-
-HEAD_REPO=$(echo "$pr_resp" | jq -r .head.repo.full_name)
-HEAD_BRANCH=$(echo "$pr_resp" | jq -r .head.ref)
-
-git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
-git config --global user.email "actions@github.com"
-git config --global user.name "GitHub Merge Action"
-
-set -o xtrace
-
-git fetch origin $HEAD_BRANCH
-
-# do the merge
-git checkout -b $HEAD_BRANCH origin/$HEAD_BRANCH
-git merge $BRANCH_TO_MERGE --no-edit
-git push origin $HEAD_BRANCH
