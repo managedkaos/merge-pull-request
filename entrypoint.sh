57ea3d137c6200382fd8a9bbe38d160a1d792f27
Michael <managedkaos@users.noreply.github.com>
2019-04-11 22:34:47 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,39 @@ echo "-----------------------------------------------------------"
 echo "Processing $GITHUB_EVENT_PATH"
 echo "-----------------------------------------------------------"
 
-echo "### printing environment"
+echo "### Printing environment"
 env
-jq . < $GITHUB_EVENT_PATH
 
-echo "### checking for PR URL"
+echo "### Printing $GITHUB_EVENT_PATH"
+jq . "$GITHUB_EVENT_PATH"
+
+echo "### Making sure this is a pull prequest by looking for the PR URL"
 (jq -r ".pull_request.issue.href" "$GITHUB_EVENT_PATH") || exit 78
 
-echo "### getting the PR number"
+echo "### Getting the PR number"
 PR_NUMBER=$(jq -r ".number" "$GITHUB_EVENT_PATH")
 
-echo "### $PR_NUMBER"
+echo "### PR_NUMBER = $PR_NUMBER"
+
+echo "### Getting the head branch"
+HEAD_BRANCH=$(jq -r .pull_request.base.repo.default_branch "$GITHUB_EVENT_PATH")
+
+echo "### Getting branch to merge"
+BRANCH_TO_MERGE=$(jq -r .pull_request.head.ref "$GITHUB_EVENT_PATH")
+
+echo "### HEAD_BRANCH     = $HEAD_BRANCH"
+echo "### BRANCH_TO_MERGE = $BRANCH_TO_MERGE"
+
+echo "### Merging the Pull Request"
+git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/${GITHUB_REPOSITORY}.git
+git config --global user.email "actions@github.com"
+git config --global user.name "GitHub Merge Action"
+
+set -o xtrace
+
+git fetch origin $HEAD_BRANCH
+
+# do the merge
+git checkout -b $HEAD_BRANCH origin/$HEAD_BRANCH
+git merge $BRANCH_TO_MERGE --no-edit
+git push origin $HEAD_BRANCH
